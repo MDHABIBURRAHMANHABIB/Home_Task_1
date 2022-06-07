5de76597dd91386eadda9074bfe71db6087fd9a9
MDHABIBURRAHMANHABIB <101259501+MDHABIBURRAHMANHABIB@users.noreply.github.com>
2022-06-07 10:56:50 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+'''Q1: Sheikh have 20 emails, He deleted 4. Then he forwarded the rest of the emails to
+four persons dividing equally. How many emails got each of them? Design code of your
+own.'''
+
+print ("Home Task by Habib \n")
+print ("01\n")
+print ("Sheikh's,")
+print ("Emails: 20")
+print ("Deleted: 04")
+print ("Remained Emails: (20-4)")
+print (" =16")
+print ("He forwarded the rest of the emails to four persons dividing them equally\n")
+print ("So,")
+print ("Each of them got email: (16/4)")
+print (" = 4\n\n")
+
+
+'''Q2: Type your Name and Birth date. Show the output which can calculate Your age.'''
+
+print ("02\n")
+print ("Hello,")
+print ("I am Habib")
+print ("Birth Date: 23 December 1996")
+print ("So, in that case, my age calculator is (2022-1996)")
+print (" = 26\n\n")
+
+'''Q3: There were 20 cyclones in the Bay of Bengal in last 5 years from September to 
+February each year. Suppose, each month have equal cyclones each year. Calculate the 
+total cyclone of January month for last 5 years. Design your own code.'''
+
+print ("03\n")
+print ("Here we have got,")
+print ("20 cyclones in the Bay of Bengal in the last 5 years from September to February ")
+print ("So,")
+print (" Each year for six months we have 4 cyclones")
+print (" Each year for one month we have 4/6 cyclones")
+print (" =.67")
+print ("So, the total cyclone of January month for the last 5 years is: (5*0.67)")
+print (" = 3.34")
